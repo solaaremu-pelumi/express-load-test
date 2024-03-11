a74b06e2137df8c4c7ef16254bcb76a71196c070
Oluwapelumi Sola-Aremu <solaaremu.pelumi@gmail.com>
2024-03-11 23:37:48 +0000

    going merry

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require('express')
+const app = express()
+
+app.get('/', function (req, res) {
+  res.send('Hello World')
+})
+
+app.get('/ping', function (req, res) {
+  res.send('pong')
+})
+
+app.listen(3000)
---
+++ readme.md
@@ -0,0 +1,3 @@
+*** Sample Node Application ***
+
+This is a sample node application to test the ability to handle multiple requests
