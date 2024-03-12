131224667ac23ab39b4403c38f1fd201ddca1b68
Oluwapelumi Sola-Aremu <solaaremu.pelumi@gmail.com>
2024-03-12 00:05:07 +0000

    adding factorial function

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
 const express = require('express')
 const app = express()
 
-app.get('/', function (req, res) {
-  res.send('Hello World')
-})
-
 app.get('/ping', function (req, res) {
   res.send('pong')
+})
+
+app.get('/factorial', function (req, res) {
+  for (let i = 1; i <= 10; i++) {
+    result *= i;
+  }
+  res.send(result)
 })
 
 app.listen(3000)
